0a31fc3ca412f3c010003ce3d63233ec44d92c9c
nickypangers <nixonpang98@gmail.com>
2021-09-11 01:38:12 +0800

    remove filelocation package, added lint task

--- pkg/storage/memory/country.go
+++ pkg/storage/memory/country.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 )
 
-// const fileDir = "/data/country-code.csv"
 const rawFile = "https://gist.githubusercontent.com/nickypangers/bedd1dafe84a97d163e157575843346e/raw/b2ff15d9df8ed76b3285660b54bfe6a0ae055bce/country-code.csv"
 
 var (
@@ -34,44 +33,9 @@ func InitCountryData() error {
 
 }
 
-// func openCountryDataSource() ([][]string, error) {
-// 	resp, err := http.Get(rawFile)
-// 	if err != nil {
-// 		return [][]string{}, nil
-// 	}
-
-// 	defer resp.Body.Close()
-// }
-
 func loadCountryData() error {
 
 	log.Println("loading country data")
-
-	// filePath = filelocation.GetParentDir("bin") + filePath
-
-	// log.Println("file path:", filePath)
-
-	// isFileExist := filelocation.IsFileExist(filePath)
-	// if !isFileExist {
-	// 	return errors.New("file does not exist: " + filePath)
-	// }
-
-	// absFilePath, err := filepath.Abs(filePath)
-	// if err != nil {
-	// 	return errors.New("cannot get absolute file path: " + absFilePath)
-	// }
-
-	// f, err := os.Open()
-	// if err != nil {
-	// 	return errors.New("unable to open file: " + filePath)
-	// }
-
-	// defer f.Close()
-
-	// lines, err := csv.NewReader(f).ReadAll()
-	// if err != nil {
-	// 	return errors.New("unable to read file")
-	// }
 
 	resp, err := http.Get(rawFile)
 	if err != nil {
