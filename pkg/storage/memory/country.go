f9b3552efbc05171b649057e20b9d7cc52d27b46
Nixon <nixon.p@mosaicdigi.com>
2021-09-10 18:12:08 +0800

    add country-code data to bin folder

--- pkg/storage/memory/country.go
+++ pkg/storage/memory/country.go
@@ -6,14 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
-
-	"github.com/nickypangers/passport-visa-api/pkg/filelocation"
 )
 
 // const fileDir = "/data/country-code.csv"
-const fileDir = "/data/country-code.csv"
+const fileDir = "country-code.csv"
 
 var (
 	countryList []string
@@ -41,23 +38,23 @@ func loadCountryData(filePath string) error {
 
 	log.Println("loading country data")
 
-	filePath = filelocation.GetParentDir("bin") + filePath
+	// filePath = filelocation.GetParentDir("bin") + filePath
 
-	log.Println("file path:", filePath)
+	// log.Println("file path:", filePath)
 
-	isFileExist := filelocation.IsFileExist(filePath)
-	if !isFileExist {
-		return errors.New("file does not exist: " + filePath)
-	}
+	// isFileExist := filelocation.IsFileExist(filePath)
+	// if !isFileExist {
+	// 	return errors.New("file does not exist: " + filePath)
+	// }
 
-	absFilePath, err := filepath.Abs(filePath)
-	if err != nil {
-		return errors.New("cannot get absolute file path: " + absFilePath)
-	}
+	// absFilePath, err := filepath.Abs(filePath)
+	// if err != nil {
+	// 	return errors.New("cannot get absolute file path: " + absFilePath)
+	// }
 
 	f, err := os.Open(filePath)
 	if err != nil {
-		return errors.New("unable to open file: " + absFilePath)
+		return errors.New("unable to open file: " + filePath)
 	}
 
 	defer f.Close()
