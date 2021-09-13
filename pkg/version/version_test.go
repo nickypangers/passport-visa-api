c0a4c1f158d1af247d9f7d2e69f1b03ad56df205
Nixon <nixon.p@mosaicdigi.com>
2021-09-13 19:10:09 +0800

    added constants authtoken and tokenonly

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestGetVersion(t *testing.T) {
 	version := APIVersion.String()
-	if version != "Version: 1.0.0" {
-		t.Fatalf("get version = %q, want match for Version: 1.0.0", version)
+	if version != "Version: 1.1.0" {
+		t.Fatalf("get version = %q, want match for Version: 1.1.0", version)
 	}
 
 }
